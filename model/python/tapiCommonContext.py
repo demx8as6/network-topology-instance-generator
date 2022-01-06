3a8c3dfb728c13b95731b61261bb75b709cb5d51
demx8as6 <martin.skorupski@highstreet-technologies.com>
2022-01-06 08:44:29 +0100

    update copyright year

--- model/python/tapiCommonContext.py
+++ model/python/tapiCommonContext.py
@@ -1,4 +1,4 @@
-# Copyright 2021 highstreet technologies GmbH
+# Copyright 2022 highstreet technologies GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 #!/usr/bin/python
 
-# Copyright 2021 highstreet technologies GmbH
+# Copyright 2022 highstreet technologies GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
